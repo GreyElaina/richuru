d64ec219109c494b6c13f33f47bfef1a6e7c12b2
Elaina <GreyElaina@outlook.com>
2024-11-16 21:34:20 +0800

    fix: ref InterceptHandler from loguru docs

--- richuru.py
+++ richuru.py
@@ -19,20 +19,20 @@ for lv in Core().levels.values():
 
 class LoguruHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
+        level: str | int
         try:
             level = logger.level(record.levelname).name
         except ValueError:
-            level = str(record.levelno)
+            level = record.levelno
 
-        frame, depth = logging.currentframe(), 2
-        while frame and frame.f_code.co_filename == logging.__file__:
+        # Find caller from where originated the logged message.
+        frame, depth = logging.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
             frame = frame.f_back
             depth += 1
 
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level,
-            record.getMessage(),
-        )
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
 
 
 def highlight(style: str) -> Dict[str, Callable[[Text], Text]]:
